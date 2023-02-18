66cec6149c915be30e42d28713f9e8da268277af
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2023-02-18 11:52:03 +0800

    Fix mixed up indentations

--- main.go
+++ main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	"fmt"
+    "fmt"
     "net/http"
-	"log"
-	"os"
+    "log"
+    "os"
     "time"
 
-	"github.com/PaulSonOfLars/gotgbot/v2"
-	"github.com/PaulSonOfLars/gotgbot/v2/ext"
-	"github.com/PaulSonOfLars/gotgbot/v2/ext/handlers"
+    "github.com/PaulSonOfLars/gotgbot/v2"
+    "github.com/PaulSonOfLars/gotgbot/v2/ext"
+    "github.com/PaulSonOfLars/gotgbot/v2/ext/handlers"
 )
 
 func check(err error, message string) {
