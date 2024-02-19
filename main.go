15dda49236d0f89c10fa2d0428dcf106ab681f85
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2024-02-19 15:25:53 +0800

    Fixup!
    Change-Id: I336c3726965840c14f6feebfc8f3b04be0dbe5d2

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-    "net/http"
     "log"
     "os"
     "time"
@@ -20,41 +19,38 @@ func main() {
     }
     var TOKEN string = string(token[0:len(token)-1]) // remove trailing \n
 
-    // create new bot
-    bot, err := gotgbot.NewBot(string(TOKEN), &gotgbot.BotOpts{
-        Client: http.Client{},
-        DefaultRequestOpts: &gotgbot.RequestOpts{
-            Timeout: gotgbot.DefaultTimeout,
-            APIURL: gotgbot.DefaultAPIURL,
-        },
-    })
+    botopts := gotgbot.BotOpts{}
+    bot, err := gotgbot.NewBot(string(TOKEN), &botopts)
     check(err, "Failed to create bot")
 
     // create updater and dispatcher
-    updater := ext.NewUpdater(&ext.UpdaterOpts{
-        Dispatcher: ext.NewDispatcher(&ext.DispatcherOpts{
-            Error: func(bot *gotgbot.Bot, context *ext.Context, err error) ext.DispatcherAction {
-                log.Println("An error occured while handling update: ", err.Error())
-                return ext.DispatcherActionNoop
-            },
-            MaxRoutines: ext.DefaultMaxRoutines,
-        }),
-    })
+    dispatcheropts := ext.DispatcherOpts{
+        Error: func(b *gotgbot.Bot, ctx *ext.Context, err error) ext.DispatcherAction {
+            // log the error and do nothing
+            log.Println("Error while handling update: ", err.Error())
+            return ext.DispatcherActionNoop
+        },
+        MaxRoutines: ext.DefaultMaxRoutines,
+    }
+    getupdatesopts := gotgbot.GetUpdatesOpts{
+        Timeout: 60,
+        RequestOpts: &gotgbot.RequestOpts{
+            // such high value to make this stfu:
+            // 2023/02/18 02:30:10 Failed to get updates; sleeping 1s: failed to execute POST request to getUpdates: Post "https://api.telegram.org/bot[REDACTED]/getUpdates": context deadline exceeded
+            Timeout: time.Second * 120,
+        },
+    }
+    pollingopts := ext.PollingOpts{
+        DropPendingUpdates: true,
+        GetUpdatesOpts: &getupdatesopts,
+    }
 
-    dispatcher := updater.Dispatcher
+    dispatcher := ext.NewDispatcher(&dispatcheropts)
+    updater := ext.NewUpdater(dispatcher, nil)
+
     dispatcher.AddHandler(handlers.NewCommand("start", cmd_start))
 
-    err = updater.StartPolling(bot, &ext.PollingOpts{
-        DropPendingUpdates: true,
-        GetUpdatesOpts: gotgbot.GetUpdatesOpts{
-            Timeout: 60,
-            RequestOpts: &gotgbot.RequestOpts{
-                // such high value to make this stfu:
-                // 2023/02/18 02:30:10 Failed to get updates; sleeping 1s: failed to execute POST request to getUpdates: Post "https://api.telegram.org/bot[REDACTED]/getUpdates": context deadline exceeded
-                Timeout: time.Second * 120,
-            },
-        },
-    })
+    err = updater.StartPolling(bot, &pollingopts)
     check(err, "Failed to start polling")
 
     log.Printf("%s has been started", bot.User.Username)
